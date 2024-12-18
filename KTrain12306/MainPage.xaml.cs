ca967c15d90978f27a45fdebcb847b22df26de7a
jiehuan-jason <jiehuan233@outlook.com>
2024-12-18 20:06:38 +0800

    添加了TrainsListPage

--- KTrain12306/MainPage.xaml.cs
+++ KTrain12306/MainPage.xaml.cs
@@ -168,9 +168,45 @@ namespace KTrain12306
             Frame.Navigate(typeof(StationChoosePage), StationQueryInfo.query_label.To);
         }
 
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
+            //Create an HTTP client object
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
 
+            //Add a user-agent header to the GET request. 
+            var headers = httpClient.DefaultRequestHeaders;
+
+            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
+            //especially if the header value is coming from user input.
+            string header = "ie";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            Uri requestUri = new Uri("https://kyfw.12306.cn/otn/leftTicketPrice/query?leftTicketDTO.train_date="+ calendar.Date.Value.ToString("yyyy-MM-dd") + "&leftTicketDTO.from_station="+from_station.station_telecode+ "&leftTicketDTO.to_station="+ to_station.station_telecode + "&leftTicketDTO.ticket_type=1&randCode=");
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
